881d039968d94cfc9c81f155f447400ae3f13e46
nk2ge5k <nk2ge5k@gmail.com>
2016-07-06 12:19:38 +0300

    Add: DynamicTextCampaign enums

---
+++ services/campaigns/enum/DynamicTextCampaignFieldEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace directapi\services\campaigns\enum;
+
+
+use directapi\components\Enum;
+
+class DynamicTextCampaignFieldEnum extends Enum
+{
+    const BiddingStrategy = 'BiddingStrategy';
+    const Settings = 'Settings';
+    const CounterIds = 'CounterIds';
+}
---
+++ services/campaigns/enum/DynamicTextCampaignNetworkStrategyTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace directapi\services\campaigns\enum;
+
+
+use directapi\components\Enum;
+
+class DynamicTextCampaignNetworkStrategyTypeEnum extends Enum
+{
+    const SERVING_OFF = 'SERVING_OFF';
+    const UNKNOWN = 'UNKNOWN';
+}
