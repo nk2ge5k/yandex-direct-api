f590b93a09570840486d0dc22de642fa61cc92f2
nk2ge5k <nk2ge5k@gmail.com>
2016-07-06 18:35:14 +0300

    do not look

--- common/criterias/LimitOffset.php
+++ common/criterias/LimitOffset.php
@@ -15,4 +15,13 @@ class LimitOffset extends Model
      * @var int
      */
     public $Offset;
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @return LimitOffset
+     */
+    public static function init( $offset = 0, $limit = 1000 ) {
+        return new LimitOffset(['Limit' => $limit, 'Offset' => $offset]);
+    }
 }
