1e1a7edea69094606235e216c4c7dee97d549072
nk2ge5k <nk2ge5k@gmail.com>
2016-07-11 16:05:57 +0300

    Add: CalloutFieldEnum

---
+++ services/adextensions/enum/CalloutFieldEnum.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nk2ge5k
+ * Date: 11.07.16
+ * Time: 16:04
+ */
+
+namespace directapi\services\adextensions\enum;
+
+
+class CalloutFieldEnum
+{
+    const CalloutText = 'CalloutText';
+}
