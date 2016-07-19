3af61639314ec7f64849cb0ccab5ae62808cc7ff
nk2ge5k <nk2ge5k@gmail.com>
2016-07-19 16:08:35 +0300

    + AdStateEnum

---
+++ services/ads/enum/AdStateEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace directapi\services\ads\enum;
+
+
+use directapi\components\Enum;
+
+class AdStateEnum extends Enum
+{
+    const OFF = 'OFF';
+    const ON = 'ON';
+    const SUSPENDED = 'SUSPENDED';
+    const OFF_BY_MONITORING = 'OFF_BY_MONITORING';
+    const ARCHIVED = 'ARCHIVED';
+    const UNKNOWN = 'UNKNOWN';
+}
