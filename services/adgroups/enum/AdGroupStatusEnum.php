c2435c2ff15e877beb3dd5dc049c73784cd52386
nk2ge5k <nk2ge5k@gmail.com>
2016-08-04 13:11:02 +0300

    Fix: AdGroupStatusEnum

--- services/adgroups/enum/AdGroupStatusEnum.php
+++ services/adgroups/enum/AdGroupStatusEnum.php
@@ -10,9 +10,6 @@ class AdGroupStatusEnum extends Enum
     const DRAFT = 'DRAFT';
     const MODERATION = 'MODERATION';
     const PREACCEPTED = 'PREACCEPTED';
-    const ACCEPTED = 'ACCEPTED';//PREACCEPTED
+    const ACCEPTED = 'ACCEPTED';
     const REJECTED = 'REJECTED';
-    const ARCHIVED = 'ARCHIVED';
-    const SUSPENDED = 'SUSPENDED';
-    const ALL = 'all';
 }
