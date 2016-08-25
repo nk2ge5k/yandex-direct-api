206055ab9b767261f755d1093e8ae119065ecd31
nk2ge5k <nk2ge5k@gmail.com>
2016-08-25 11:41:52 +0300

    Add: AdImage enums

---
+++ services/ads/enum/MobileAppImageAdFieldEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace directapi\services\ads\enum;
+
+
+use directapi\components\Enum;
+
+class MobileAppImageAdFieldEnum extends Enum
+{
+    const AdImageHash = 'AdImageHash';
+    const TrackingUrl = 'TrackingUrl';
+}
---
+++ services/ads/enum/TextImageAdFieldEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace directapi\services\ads\enum;
+
+
+use directapi\components\Enum;
+
+class TextImageAdFieldEnum extends Enum
+{
+    const AdImageHash = 'AdImageHash';
+    const Href = 'Href';
+}
