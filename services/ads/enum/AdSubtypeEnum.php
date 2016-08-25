6e460206561a9c602495e1498154a72440fdb931
nk2ge5k <nk2ge5k@gmail.com>
2016-08-25 12:05:58 +0300

    Add: AdSubtypeEnum

---
+++ services/ads/enum/AdSubtypeEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace directapi\services\ads\enum;
+
+
+use directapi\components\Enum;
+
+class AdSubtypeEnum extends Enum
+{
+    const NONE = 'NONE';
+    const MOBILE_APP_IMAGE_AD = 'MOBILE_APP_IMAGE_AD';
+    const TEXT_IMAGE_AD = 'TEXT_IMAGE_AD';
+}
