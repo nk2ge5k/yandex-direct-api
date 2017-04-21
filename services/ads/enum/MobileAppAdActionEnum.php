627257fbbfd8eeedaa2f269982165e20dde440d3
nk2ge5k <nk2ge5k@gmail.com>
2017-04-21 10:48:37 +0300

    Update enum

--- services/ads/enum/MobileAppAdActionEnum.php
+++ services/ads/enum/MobileAppAdActionEnum.php
@@ -7,4 +7,11 @@ use directapi\components\Enum;
 class MobileAppAdActionEnum extends Enum
 {
     const DOWNLOAD = 'DOWNLOAD';
+    const GET = 'GET';
+    const INSTALL = 'INSTALL';
+    const MORE = 'MORE';
+    const OPEN = 'OPEN';
+    const UPDATE = 'UPDATE';
+    const PLAY = 'PLAY';
+    const BUY_AUTODETECT = 'BUY_AUTODETECT';
 }
